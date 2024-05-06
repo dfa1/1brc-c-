c22c72b0f0523333b0f53d8d6cbebd50008e8fd7
Davide Angelocola <davide.angelocola@gmail.com>
2024-05-06 14:53:09 +0200

    reduce memory allocation (1m52s)
    Rather than storing all the possible values in a std::vector,
    let's use a single aggregation on the current temperature.
    
    Memory consumption is reduced a lot (from 10GB to 1MB).

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <numeric>
 #include <string>
 #include <unordered_map>
@@ -15,13 +16,23 @@ inline float parse_float(const char *start, const char *end) {
   return result;
 }
 
-struct Result {
-  std::string city;
-  float min;
-  float max;
-  float avg;
+class Temperature {
+  float min_ = 9999;
+  float max_ = -9999;
+  float sum_ = 0;
+  int count_ = 0;
 
-  bool operator<(const Result &that) { return this->city < that.city; }
+ public:
+  void merge(const float value) {
+    min_ = std::min(value, min_);
+    max_ = std::max(value, max_);
+    sum_ += value;
+    count_ += 1;
+  }
+
+  float min() const { return min_; }
+  float max() const { return max_; }
+  float avg() const { return sum_ / count_; }
 };
 
 int main() {
@@ -29,42 +40,22 @@ int main() {
 
   std::ifstream infile("measurements.txt");
   std::string line;
-  std::unordered_map<std::string, std::vector<float>> by_city;
+  std::unordered_map<std::string, Temperature> by_city;
   while (std::getline(infile, line)) {
     const std::size_t semicolon = line.find_first_of(';');
     if (semicolon != std::string::npos) {
       const std::string city = line.substr(0, semicolon);
       const float value =
           parse_float(line.data() + semicolon + 1, line.data() + line.size());
-      by_city[city].push_back(value);
+      by_city[city].merge(value);
     }
   }
-  std::vector<Result> sorted_by_city;
-  sorted_by_city.reserve(by_city.size());
-  for (const auto &[city, temps] : by_city) {
-    float min = 99999;
-    float max = -99999;
-    float sum = 0;
-
-    for (const auto &temp : temps) {
-      min = std::min(min, temp);
-      max = std::max(max, temp);
-      sum += temp;
-    }
-
-    Result result;
-    result.city = city;
-    result.min = min;
-    result.max = max;
-    result.avg = sum / temps.size();
-
-    sorted_by_city.push_back(result);
+  std::map<std::string, Temperature> sorted_by_city;
+  for (const auto &[city, temperature] : by_city) {
+    sorted_by_city[city] = temperature;
   }
-
-  std::sort(sorted_by_city.begin(), sorted_by_city.end());
-
-  for (const Result &result : sorted_by_city) {
-    std::cout << result.city << ";" << result.min << ";" << result.max << ";"
-              << result.avg << "\n";
+  for (const auto &[city, temperature] : sorted_by_city) {
+    std::cout << city << ";" << temperature.min() << ";" << temperature.max()
+              << ";" << temperature.avg() << "\n";
   }
 }
